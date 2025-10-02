992010219ef91284ea8b9bb474b42fe1d4fc669a
renickmaina <147622708+renickmaina@users.noreply.github.com>
2025-10-02 18:36:39 +0300

    Update insert_books.js
    changed the connection URI

--- insert_books.js
+++ insert_books.js
@@ -3,8 +3,10 @@
 // Import MongoDB client
 const { MongoClient } = require('mongodb');
 
-// Connection URI (replace with your MongoDB connection string if using Atlas)
-const uri = 'mongodb://localhost:27017';
+// Connection URI 
+const uri = process.env.MONGO_URI;
+
+
 
 // Database and collection names
 const dbName = 'plp_bookstore';
